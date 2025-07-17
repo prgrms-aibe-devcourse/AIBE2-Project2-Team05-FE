7e1253411c378f4341680c33a18d5a94c74e7b32
Minjae Kim <167865311+Lotusrious@users.noreply.github.com>
2025-07-17 15:05:51 +0900

    feat : RoutePage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import AuthPage from './pages/AuthPage';
+import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<AuthPage />} />
+      <Route path="/" element={<ProtectedRoute />}>
+        <Route index element={<Dashboard />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
